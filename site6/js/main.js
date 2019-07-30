114641421aeaba79fcb2c75cfdc8db0f4da0223d
sanua356 <sanek.pankratov356@gmail.com>
2019-07-30 12:28:15 +0300

    site6

---
+++ site6/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.visually-impaired-icon').on('click', function(){
+		$('.acessability-bg').css('display', 'block')
+	});
+});	
+$(document).ready(function() {
+	$('.exit').on('click', function(){
+		$('.acessability-bg').css('display', 'none')
+	});
+});	
