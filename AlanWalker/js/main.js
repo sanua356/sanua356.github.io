7877979aa7983f4f19565c0b01e6e392e3988c10
sanua356 <sanek.pankratov356@gmail.com>
2019-10-04 15:36:41 +0300

    123

--- AlanWalker/js/main.js
+++ AlanWalker/js/main.js
@@ -156,31 +156,29 @@ $(document).ready(function(){
 //AOS Animate	
   	AOS.init()
 	AOS.init({
-		disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-		startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-		initClassName: 'aos-init', // class applied after initialization
-		animatedClassName: 'aos-animate', // class applied on animation
-		useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-		disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-		debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-		throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+		disable: false,
+		startEvent: 'DOMContentLoaded',
+		initClassName: 'aos-init',
+		animatedClassName: 'aos-animate',
+		useClassNames: false,
+		disableMutationObserver: false,
+		debounceDelay: 50,
+		throttleDelay: 99,
 		  
-
-		  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-		offset: 120, // offset (in px) from the original trigger point
-		delay: 0, // values from 0 to 3000, with step 50ms
-		duration: 600, // values from 0 to 3000, with step 50ms
-		easing: 'ease', // default easing for AOS animations
-		once: false, // whether animation should happen only once - while scrolling down
-		mirror: false, // whether elements should animate out while scrolling past them
-		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation);
+		offset: 120,
+		delay: 0,
+		duration: 600,
+		easing: 'ease',
+		once: false,
+		mirror: false,
+		anchorPlacement: 'top-bottom',
 	});
 });	
 //Hamburger-menu-animate
 $(document).ready(function() {
 	$(".hamburger-menu").on("click", function() {
 		$("li").toggleClass("hide");
-		$("li").toggleClass("animate-hamburger");
+		$("li").toggleClass("opct-animate");
 	});
 	  if ( $(window).width() <= 768 ) {
       $("li").addClass("hide");
@@ -188,3 +186,48 @@ $(document).ready(function() {
     }
 });
 //Hamburger-menu-animate-end
+//View-all-tours
+$(document).ready(function() {
+	$("#view-all-tours-btn").on("click", function() {
+		$("#view-all-tours").addClass("hide");
+		$(".shows-tours").removeClass("hide");
+		$(".shows-tours").removeClass("opct-rv-animate");
+		$(".shows-tours").addClass("opct-animate");
+		$("#hide-all-tours").removeClass("hide");
+	});
+});
+//View-all-tours-end
+//hide-all-tours
+$(document).ready(function() {
+	$("#hide-all-tours-btn").on("click", function() {
+		$(".shows-tours").removeClass("opct-animate");
+		$(".shows-tours").addClass("opct-rv-animate");
+		setTimeout('$(".shows-tours").addClass("hide");$("#hide-all-tours").addClass("hide");', 1000);
+		$("#view-all-tours").removeClass("hide");		
+	});
+});
+//hide-all-tours-end
+$(document).ready(function(){
+  $('.slider-desc').slick({
+  	nextArrow: '#prev-arrow',
+  	prevArrow: '#next-arrow',
+  	dotsClass: 'slider-active-number',
+  	});
+});
+$(document).ready(function(){
+	if ( $(".slick-active").hasClass("slide1") ) {
+		$("#current-slide").empty()
+		$('#current-slide').append("1");
+	}
+	else{}
+	if ( $(".slick-active").hasClass("slide2") ) {
+			$("#current-slide").empty()
+			$('#current-slide').append("2");
+		}
+	else{}
+	if ( $(".slick-active").hasClass("slide3") ) {
+			$("#current-slide").empty()
+			$('#current-slide').append("3");
+		}
+	else{}
+});
